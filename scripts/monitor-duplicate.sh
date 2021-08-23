2cb99c555029c247b750737dc7e8010841c37d5f
Retchut <retchut@protonmail.ch>
2021-08-23 12:23:04 +0100

    feat: create monitor extend above script

--- scripts/monitor-duplicate.sh
+++ scripts/monitor-duplicate.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
-#mirror monitor 2 to monitor 1, where monitor 2 will be scaled so the image has monitor 1's resolution
-xrandr --output [monitor 1] --mode [resx1]x[resy1] --output [monitor 2] --same-as [monitor 1] --scale `echo "[resx1]/[resx2]" | bc -l`x`echo "[resy1]/[resy2]" | bc -l`
+#mirror monitor 1 to monitor 2, where monitor 1 will be scaled so the image has monitor 2's resolution
+xrandr --output [monitor 2] --mode [res2x]x[res2y] --output [monitor 1] --same-as [monitor 2] --scale `echo "[res2x]/[res1x]" | bc -l`x`echo "[res2y]/[res1y]" | bc -l`
 ~/.fehbg
---
+++ scripts/monitor-extend-above.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#extend monitor 2 above monitor 1, both with the desired resolution and not scaled
+xrandr --output [monitor 1] --mode [res1x]x[res1y] --scale 1 --output [monitor 2] --mode [res2x]x[res2y] --above [monitor 1] --scale 1
+~/.fehbg
